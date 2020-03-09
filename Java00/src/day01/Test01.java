c7a5f0efab2f6486de48588393396f68bd0f2d8b
class02 <class02@DESKTOP-4772329>
2020-03-09 15:06:55 +0900

    test01 1st commit

---
+++ Java00/src/day01/Test01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("Hello Lava!!");
+		// ==> println() 함수(method) - 콘솔창에 내용을 출력해주는 기능을 가진 함수
+		// 이클립스에서 자바클래스 실행하기
+		// Ctrl + Shift + F11
+	}
+}
